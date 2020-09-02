fe4c11b34aa51e1e5cdf4bc6aa3ddb0273487171
Churiphonthongsawat <churiphon.th.61@ubu.ac.th>
2020-09-02 19:52:59 +0700

    Updates

---
+++ precache-manifest.b037d553b506f51e18b0b43569e7e79d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13d3816f272859b7b5ac5ffd9600756",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "40a172e28837f1c102b9",
+    "url": "/tic-tac-toe/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a172e28837f1c102b9",
+    "url": "/tic-tac-toe/static/js/main.88c5dacb.chunk.js"
+  },
+  {
+    "revision": "4ad95f08d6b4d56d4069",
+    "url": "/tic-tac-toe/static/js/runtime-main.fa7aabe4.js"
+  }
+]);
